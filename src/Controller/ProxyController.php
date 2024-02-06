1ac2e2c7d29f94179a5fcc165e3d8890d0b3c7ea
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-02-06 15:16:24 +0100

    use central outputJson

--- src/Controller/ProxyController.php
+++ src/Controller/ProxyController.php
@@ -30,6 +30,7 @@ use OxidSolutionCatalysts\PayPal\Core\PayPalDefinitions;
 use OxidSolutionCatalysts\PayPal\Core\PayPalSession;
 use OxidSolutionCatalysts\PayPal\Core\ServiceFactory;
 use OxidSolutionCatalysts\PayPal\Core\Utils\PayPalAddressResponseToOxidAddress;
+use OxidSolutionCatalysts\PayPal\Traits\JsonTrait;
 use OxidSolutionCatalysts\PayPalApi\Model\Orders\Order as PayPalApiOrder;
 use OxidSolutionCatalysts\PayPalApi\Model\Orders\OrderRequest;
 
@@ -38,6 +39,7 @@ use OxidSolutionCatalysts\PayPalApi\Model\Orders\OrderRequest;
  */
 class ProxyController extends FrontendController
 {
+    use JsonTrait;
     use ServiceContainer;
 
     public function createOrder()
@@ -167,18 +169,6 @@ class ProxyController extends FrontendController
         PayPalSession::unsetPayPalOrderId();
         Registry::getSession()->getBasket()->setPayment(null);
         Registry::getUtils()->redirect(Registry::getConfig()->getShopSecureHomeURL() . 'cl=payment', false, 301);
-    }
-
-    /**
-     * Encodes and sends response as json
-     *
-     * @param $response
-     */
-    protected function outputJson($response)
-    {
-        $utils = Registry::getUtils();
-        $utils->setHeader('Content-Type: application/json');
-        $utils->showMessageAndExit(json_encode($response));
     }
 
     protected function addToBasket($qty = 1): void
