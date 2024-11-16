OXID-eSales/paypal-module

PSPAYPAL-680 - fix links not working when only one lang is active in the shop

views/tpl/shared/page/account/account_vaulting_paypal.tpl
@@ -18,7 +18,7 @@
             paypal.Buttons({
                                createVaultSetupToken: async () => {
                                    const result = await fetch(
-                                       "[{oxgetseourl ident=$oViewConf->getSslSelfLink()}]&cl=osctokencontroller&fnc=generatesetuptoken&XDEBUG_SESSION_START=1",
+                                       "[{oxgetseourl ident=$oViewConf->getGenerateSetupTokenLink()}]",
                                        { method: "POST"
                                        })
                                    const { id } = await result.json();
@@ -26,7 +26,7 @@
                                },
                                onApprove: async ({ vaultSetupToken }) => {
                                    const result = await fetch(
-                                       "[{oxgetseourl ident=$oViewConf->getSslSelfLink()}]&cl=osctokencontroller&fnc=generatepaymenttoken&XDEBUG_SESSION_START=1&token="+vaultSetupToken,
+                                       "[{oxgetseourl ident=$oViewConf->getSslSelfLink()}]"+vaultSetupToken,
                                        {method: 'POST'
                                        })
                                    const status = await result.json();

OXIDprojects/paypal-oxid

views/smarty/frontend/blocks/page/account/account_vaulting_paypal.tpl
@@ -18,7 +18,7 @@
             paypal.Buttons({
                                createVaultSetupToken: async () => {
                                    const result = await fetch(
-                                       "[{oxgetseourl ident=$oViewConf->getSslSelfLink()}]&cl=osctokencontroller&fnc=generatesetuptoken&XDEBUG_SESSION_START=1",
+                                       "[{oxgetseourl ident=$oViewConf->getGenerateSetupTokenLink()}]",
                                        { method: "POST"
                                        })
                                    const { id } = await result.json();
@@ -26,7 +26,7 @@
                                },
                                onApprove: async ({ vaultSetupToken }) => {
                                    const result = await fetch(
-                                       "[{oxgetseourl ident=$oViewConf->getSslSelfLink()}]&cl=osctokencontroller&fnc=generatepaymenttoken&XDEBUG_SESSION_START=1&token="+vaultSetupToken,
+                                       "[{oxgetseourl ident=$oViewConf->getGeneratePaymentTokenLink()}]"+vaultSetupToken,
                                        {method: 'POST'
                                        })
                                    const status = await result.json();