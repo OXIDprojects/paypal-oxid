a39c267cff2707b90e702446139054c801021443
René Gust <rene.gust@oxid-esales.com>
2024-12-16 07:00:14 +0100

    PSPAYPAL-815 add bugfix

--- tests/Codeception/Acceptance/WebhookCest.php
+++ tests/Codeception/Acceptance/WebhookCest.php
@@ -114,6 +114,8 @@ final class WebhookCest extends BaseCest
             // must be empty. We do not have the merchant's payerid
             //and confirmed by paypal we should not use it for auth and
             //so not ask for it on the configuration page
+            '',
+            '',
             false
         );
     }
